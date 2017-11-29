0176a8f42063646454023779fe91924168e38052
shashwatjv <shashwat@pdx.edu>
2017-11-28 19:57:35 -0800

    add all file except traces

---
+++ inc/predictor.h
@@ -0,0 +1,16 @@
+#ifndef _PREDICTOR_H_
+#define _PREDICTOR_H_
+
+#define TEMPORAL
+
+#define PHT_CTR_MAX  3
+#define PHT_CTR_INIT 2
+
+#define PHT_SIZE 1024*16 
+
+#define TS_KEY_SIZE (140 + 32)
+
+//#define TS_ON
+
+#endif
+
