32fab7168e5d67a64bc76e928c93f68858bffea7
shashwatjv <shashwat@pdx.edu>
2017-12-08 19:10:59 -0800

    update run script to spawn all in parallel

--- run.sh
+++ run.sh
@@ -3,27 +3,27 @@
 for i in `ls trace/ | cut -d "." -f 1`; do
 	
 	echo "Running PAG Temporal ... $i"
-	./run_champsim.sh pag_ts-no-no-lru-1core 1 50 $i
+	./run_champsim.sh pag_ts-no-no-lru-1core 1 50 $i &
 	
 	echo "Running PAP Temporal ... $i"
-	./run_champsim.sh pap_ts-no-no-lru-1core 1 50 $i
+	./run_champsim.sh pap_ts-no-no-lru-1core 1 50 $i &
 	
 	echo "Running GSHARE Temporal ... $i"
-	./run_champsim.sh gshare_ts-no-no-lru-1core 1 50 $i
+	./run_champsim.sh gshare_ts-no-no-lru-1core 1 50 $i &
 	
 	echo "Running BiModal Temporal ... $i"
-	./run_champsim.sh bimodal_ts-no-no-lru-1core 1 50 $i
+	./run_champsim.sh bimodal_ts-no-no-lru-1core 1 50 $i &
 	
 	echo "Running PAG Normal ... $i"
-	./run_champsim.sh pag-no-no-lru-1core 1 50 $i
+	./run_champsim.sh pag-no-no-lru-1core 1 50 $i &
 	
 	echo "Running PAP Normal ... $i"
-	./run_champsim.sh pap-no-no-lru-1core 1 50 $i
+	./run_champsim.sh pap-no-no-lru-1core 1 50 $i &
 	
 	echo "Running GSHARE Normal ... $i"
-	./run_champsim.sh gshare-no-no-lru-1core 1 50 $i
+	./run_champsim.sh gshare-no-no-lru-1core 1 50 $i &
 	
 	echo "Running BiModal Normal ... $i"
-	./run_champsim.sh bimodal-no-no-lru-1core 1 50 $i
+	./run_champsim.sh bimodal-no-no-lru-1core 1 50 $i &
 done	
-echo "All Done ... SO Jayo"
+echo "All Runs Spawned >> logs available in results directory"
